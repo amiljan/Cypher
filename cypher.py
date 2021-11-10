2baaba5001c370c90942810c0d29ca958b6f3ab3
neovonpoet <andro.miljan@gmail.com>
2021-11-10 21:26:49 +0100

    added more symbols

--- cypher.py
+++ cypher.py
@@ -1,11 +1,11 @@
-
 lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'č', 'r', 's', 't', 'u', 'v', 'š', 'đ', 'ž', 'z'," ",".",",","?","!","ć","0","1","2","3","4",
-"5","6","7","8","9","q","w","x","y"
+"5","6","7","8","9","q","w","x","y",'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Č', 'R', 'S', 'T', 'U', 'V', 'Š', 'Đ', 'Ž', 'Z',"Q","W","X","Y","Ć",
+"#","$","%","&","/","(",")","=","*","+","-","_",":",";","<",">","[","]","{","}","€","@","|",'"',
 ]
 
 
 def encode(tekst):
-    key = tekst.lower()
+    key = tekst
     kljuc = []
     for letter in key:
         for i in range(len(lista)):
@@ -27,8 +27,8 @@ def encrypt(codeword,message):
         count +=1
     encrypted_message = ""
     for i in range(len(new_cypher)):
-        if new_cypher[i] > 45:
-            new_cypher[i] = new_cypher[i] - 46
+        if new_cypher[i] > 100:
+            new_cypher[i] = new_cypher[i] - 101
         encrypted_message = encrypted_message + lista[new_cypher[i]]
     return encrypted_message
 
@@ -47,7 +47,7 @@ def decrypt(codeword,message):
     encrypted_message = ""
     for i in range(len(new_cypher)):
         if new_cypher[i] < 0:
-            new_cypher[i] = new_cypher[i] + 46
+            new_cypher[i] = new_cypher[i] + 101
         encrypted_message = encrypted_message + lista[new_cypher[i]]
     return encrypted_message
 
